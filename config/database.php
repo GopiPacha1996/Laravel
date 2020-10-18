f72781cfc71bc04126a4cc6e1431c328389a1af9
Gopi <59192600+GopiPacha1996@users.noreply.github.com>
2020-10-18 10:08:05 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'sample'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root@123'),
+            'password' => env('DB_PASSWORD', '12345'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
